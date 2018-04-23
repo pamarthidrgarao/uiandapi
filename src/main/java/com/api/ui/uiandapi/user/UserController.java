febf2827780735f348df817515d59ad523a6f41e
springbootapi <apispringboot@gmail.com>
2018-04-23 12:07:00 +0530

    logging added

--- src/main/java/com/api/ui/uiandapi/user/UserController.java
+++ src/main/java/com/api/ui/uiandapi/user/UserController.java
@@ -1,5 +1,7 @@
 package com.api.ui.uiandapi.user;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -13,12 +15,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class UserController {
 
+	private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
 	@Autowired
 	private UserService userService;
 
 	@PostMapping("/saveOrUpdate")
 	@ResponseBody
 	public UserResponse save(@RequestBody User user) {
+		LOG.info("save or update called on user");
 		UserResponse response = new UserResponse();
 		response.setUser(userService.save(user));
 		response.setSuccess(true);
@@ -28,15 +33,17 @@ public class UserController {
 	@GetMapping("/getAll")
 	@ResponseBody
 	public UserResponse get() {
+		LOG.info("getAll called on user");
 		UserResponse response = new UserResponse();
 		response.setUsers(userService.getAll());
 		response.setSuccess(true);
 		return response;
 	}
-	
+
 	@GetMapping("/findByMail")
 	@ResponseBody
 	public UserResponse getByMail(@RequestParam String email) {
+		LOG.info("findByMail called on user");
 		UserResponse response = new UserResponse();
 		response.setUser(userService.findByEmail(email));
 		response.setSuccess(true);
